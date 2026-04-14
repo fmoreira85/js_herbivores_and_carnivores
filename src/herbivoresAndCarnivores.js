52a1979beba1718f5387185b9cbaf41ef3b19e5d
fmoreira85 <fabiomoreiradacunha1@gmail.com>
2026-04-14 14:01:30 -0400

    primeiro commit

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,49 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  static alive = [];
+
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    Animal.alive.push(this);
+  }
+
+  takeDamage(damage) {
+    this.health -= damage;
+
+    if (this.health <= 0) {
+      Animal.alive = Animal.alive.filter((animal) => animal !== this);
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name) {
+    super(name);
+    this.hidden = false;
+  }
+
+  hide() {
+    this.hidden = !this.hidden;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  bite(animal) {
+    // Não morde carnívoro
+    if (animal instanceof Carnivore) {
+      return;
+    }
+
+    // Não morde se o herbívoro está escondido
+    if (animal.hidden) {
+      return;
+    }
+
+    // Morde e reduz health em 50
+    animal.takeDamage(50);
+  }
 }
 
 module.exports = {
